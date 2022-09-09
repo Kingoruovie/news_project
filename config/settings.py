b95d4e8bb800d399511528503d2e0a8057bd77d0
Kingoruovie <oruovie@gmail.com>
2022-09-09 01:26:23 +0100

    edited secret key in settings based on some errors for not including it

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ env = Env()
 env.read_env()
 
 DEBUG = env.bool("DEBUG", default=False)
+SECRET_KEY = env.str("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
